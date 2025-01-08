d6a7639ed39d2e72f3968b9ca22dc7808cccb6c2
Mark Paul <markpaulbusiness66@gmail.com>
2025-01-08 17:00:12 +0300

    Working on the Map Container Component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <div className="bold-text">
+        MAP WITH CLICKABLE POLY LINES
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/ClickablePolylines.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { MapContainer, TileLayer, Polyline, useMapEvents } from "react-leaflet";
+
+const ClickablePolylines = ()=>{
+    const [routes, setSoutes] = useState([
+        {
+            id:1,
+            coordinates: [
+                
+            ]
+        }
+    ])
+}
